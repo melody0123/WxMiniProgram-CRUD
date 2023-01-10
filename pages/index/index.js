d5af504adc5c75d2f9f709970e87fe2af059d40f
melody0123 <88380000+melody0123@users.noreply.github.com>
2023-01-11 07:25:18 +0800

    完成页面跳转

--- pages/index/index.js
+++ pages/index/index.js
@@ -37,6 +37,13 @@ Page({
       }
     })
   },
+
+  goToCrud: function(e) {
+    wx.navigateTo({
+      url: '/pages/crud/crud',
+    })
+  },
+
   getUserInfo(e) {
     // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
     console.log(e)
