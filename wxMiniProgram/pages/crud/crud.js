c362dbf1273c68eee49724abc8659fa39415dcc4
melody0123 <88380000+melody0123@users.noreply.github.com>
2023-01-11 09:34:04 +0800

    完成了CRUD

--- wxMiniProgram/pages/crud/crud.js
+++ wxMiniProgram/pages/crud/crud.js
@@ -31,15 +31,60 @@ Page({
   },
 
   deleteRecord: function() {
-
+    let id = 960;
+    let sql = "delete from `device_file` where id=" + id;
+    wx.request({
+      url: app.globalData.baseUrl + '/device_file_servlet_action',
+      data: {
+        'action': 'update_record',
+        'sql': sql
+      },
+      header: {
+        "content-type":"application/x-www-form-urlencoded",
+        "x-requested-with": "XMLHttpRequest"
+      },
+      success: function(res) {
+        console.log(res.data);
+      }
+    })
   },
   
   updateRecord: function() {
-
+    let id = 961;
+    let deviceName = '修改后';
+    let sql = "update `device_file` set `device_name`='" + deviceName + "' where id=" + id;
+    wx.request({
+      url: app.globalData.baseUrl + '/device_file_servlet_action',
+      data: {
+        'action': 'update_record',
+        'sql': sql
+      },
+      header: {
+        "content-type":"application/x-www-form-urlencoded",
+        "x-requested-with": "XMLHttpRequest"
+      },
+      success: function(res) {
+        console.log(res.data);
+      }
+    })
   },
 
   selectRecord: function() {
-
+    let sql = "select * from `device_file` order by `create_time`"
+    wx.request({
+      url: app.globalData.baseUrl + '/device_file_servlet_action',
+      data: {
+        'action': 'query_record',
+        'sql': sql
+      },
+      header: {
+        "content-type":"application/x-www-form-urlencoded",
+        "x-requested-with": "XMLHttpRequest"
+      },
+      success: function(res) {
+        console.log(res.data);
+      }
+    })
   },
 
   /**
