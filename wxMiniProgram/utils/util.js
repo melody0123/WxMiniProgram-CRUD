8db7549f1b0a81922d638edc9ff3735e3f7ebf5e
melody0123 <88380000+melody0123@users.noreply.github.com>
2023-01-17 14:58:53 +0800

    添加了两个页面

--- wxMiniProgram/utils/util.js
+++ wxMiniProgram/utils/util.js
@@ -1,22 +1,22 @@
-const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
+// const formatTime = date => {
+//   const year = date.getFullYear()
+//   const month = date.getMonth() + 1
+//   const day = date.getDate()
+//   const hour = date.getHours()
+//   const minute = date.getMinutes()
+//   const second = date.getSeconds()
 
-  return `${[year, month, day].map(formatNumber).join('/')} ${[hour, minute, second].map(formatNumber).join(':')}`
-}
+//   return `${[year, month, day].map(formatNumber).join('/')} ${[hour, minute, second].map(formatNumber).join(':')}`
+// }
 
-const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : `0${n}`
-}
+// const formatNumber = n => {
+//   n = n.toString()
+//   return n[1] ? n : `0${n}`
+// }
 
-module.exports = {
-  formatTime
-}
+// module.exports = {
+//   formatTime
+// }
 
 Date.prototype.format = function(format) {
   var date = {
@@ -39,3 +39,59 @@ Date.prototype.format = function(format) {
   }
   return format;
 }
+
+/**
+* 格式化时间 
+* @param {String} date 原始时间格式
+* 格式后的时间：yyyy/mm/dd hh:mm:ss
+**/
+const formatTime = (date) => {
+  var year = date.getFullYear();
+  var month = date.getMonth() + 1;
+  var day = date.getDate();
+  var hour = date.getHours();
+  var minute = date.getMinutes();
+  var second = date.getSeconds();
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+function formatNumber(n){
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
+
+/**
+* 从一个数组中随机取出若干个元素组成数组
+* @param {Array} arr 原数组
+* @param {Number} count 需要随机取得个数
+**/
+const getRandomArray = (arr, count) => {
+  var shuffled = arr.slice(0),
+      i = arr.length, 
+      min = i - count, 
+      temp, 
+      index;
+  while (i-- > min) {
+    index = Math.floor((i + 1) * Math.random());
+    temp = shuffled[index];
+    shuffled[index] = shuffled[i];
+    shuffled[i] = temp;
+  }
+  return shuffled.slice(min);
+}
+
+/**
+* 从一个数组中随机取出一个元素
+* @param {Array} arr 原数组
+**/
+const getRandomArrayElement = arr => {
+   return arr[Math.floor(Math.random()*arr.length)];
+}
+
+
+module.exports = {
+  formatTime: formatTime,
+  getRandomArray: getRandomArray,
+  getRandomArrayElement: getRandomArrayElement,
+}
+
